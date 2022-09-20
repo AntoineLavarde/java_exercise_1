4d720221ca6dd16b45c03e4750bd027febaeddf4
Antoine Lavarde <antoine.lavarde@epita.fr>
2022-09-20 14:55:35 +0200

    Adding fibo calculation in Launcher.java

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,13 +1,27 @@
 import java.util.Scanner;
 
 public class Launcher {
+    public static int fibonacci(int number) {
+        if (number == 0) {
+            return 0;
+        } else if (number == 1 || number == 2) {
+            return 1;
+        } else {
+            return fibonacci(number - 2) + fibonacci(number - 1);
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Hello, Welcome Here !");
-        System.out.println("Write whatever you want : ");
         Scanner scanner = new Scanner(System.in);
         while (true) {
+            System.out.println("Write whatever you want : ");
             String result = scanner.nextLine();
-            if (!result.equals("quit")) {
+            if ("fibo".equals(result)) {
+              System.out.println("Enter a number n to get the Fibonacci sequence at the index number n : ");
+              int input = scanner.nextInt();
+              System.out.print("F(" + input + ")" + " = " + fibonacci(input) + "\n");
+              scanner.nextLine();
+            } else if (!"quit".equals(result)) {
                 System.out.println("Unknow command");
             } else {
                 break;
